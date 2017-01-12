ec2106b49d139dff4a4852a08260e26d9afa6c31
Dan Hoying <danhoying@gmail.com>
2017-01-12 07:49:11 -0800

    Modify TicketPrice class

--- MyProject/src/TicketPrice.java
+++ MyProject/src/TicketPrice.java
@@ -6,9 +6,13 @@ public class TicketPrice {
 		Scanner keyboard = new Scanner(System.in);
 		int age;
 		double price = 0.00;
+		char reply;
 		
 		System.out.print("How old are you? ");
 		age = keyboard.nextInt();
+		
+		System.out.print("Have a coupon? (Y/N) ");
+		reply = keyboard.findWithinHorizon(".", 0).charAt(0);
 		
 		if (age >= 12 && age < 65) {
 			price = 9.25;
@@ -17,6 +21,14 @@ public class TicketPrice {
 			price = 5.25;
 		}
 		
+		if (reply == 'Y' || reply == 'y') {
+			price -= 2.00;
+		}
+		
+		if (reply != 'Y' && reply != 'y' &&
+				reply != 'N' && reply != 'n') {
+			System.out.println("Huh?");
+		}
 		System.out.print("Please pay $");
 		System.out.print(price);
 		System.out.print(". ");
